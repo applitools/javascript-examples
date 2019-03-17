179e371e82101eba2e5dbc19c35028fb62a71ff9
isonic1 <justin.ison@gmail.com>
2019-03-17 18:54:22 -0400

    added private cloud and api key override

--- Selenium4/crawler/crawler.js
+++ Selenium4/crawler/crawler.js
@@ -76,9 +76,13 @@ async function browser(url) {
    try {
       
       var eyes = new Eyes(enableVisualGrid);
-      eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
+      var key = apiKey || process.env.APPLITOOLS_API_KEY;
+      eyes.setApiKey(key);
       eyes.setLogHandler(new ConsoleLogHandler(log));
       eyes.setBatch({id: batchId, name: sitemapFile});
+      
+      var server = serverUrl || "https://eyes.applitools.com" 
+      eyes.setServerUrl(server);
       //eyes.setBatch(new BatchInfo(sitemapFile));
 
       if (headless) {
@@ -93,7 +97,11 @@ async function browser(url) {
          console.log('Navigating to Url: ', url + '\n'); 
          return sessionId;
       });
-
+      
+      var sleep = require('sleep');
+      var millSleep = Math.floor(Math.random() * Math.floor(1000));
+      sleep.msleep(millSleep);
+      
       await driver.get(url);
       
       // Batching broke with 4.9.0 :(
@@ -118,6 +126,7 @@ async function browser(url) {
          const configuration = new SeleniumConfiguration();
          configuration.setAppName(appName);
          configuration.setTestName(url);
+         configuration.concurrentSessions = 50;
          configuration.addBrowser( 500,  800, BrowserType.CHROME  );
          configuration.addBrowser( 500,  800, BrowserType.FIREFOX );
          configuration.addBrowser( 1000, 800, BrowserType.CHROME  );
@@ -138,7 +147,7 @@ async function browser(url) {
    } catch(err) {
       
       console.error('\n' + sessionId + ' Unhandled exception: ' + err.message);
-      console.log('Fiailed Test: ', url + '\n'); 
+      console.log('Failed Test: ', url + '\n'); 
     
       if (driver && sessionId) {
          await driver.quit();
@@ -155,14 +164,26 @@ async function browser(url) {
 
 }
 
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
 const promiseProducer = () => {
    
    if (array.length === 0) {
+      // var finished = new Date();
+      // var diff = Math.abs(start - finished);
+      // var duration = millisToMinutesAndSeconds(diff);
+      // console.log("\nTotal Duration: " + duration);
       return null;
+   } else {
+      console.log("\nURLs Remaining: " + array.length + "\n")
    }
    
    const url = array.pop();
-   
+      
    return new Promise((resolve) => {
       browser(url).then(function (url) {
          resolve(url);
@@ -184,11 +205,14 @@ function isInt(value) {
 var batchId = Math.round((new Date()).getTime() / 1000).toString();
 console.log("My Applitools Batch ID: " + batchId)
 
+let apiKey = String;
+let serverUrl = String;
 let enableVisualGrid = Boolean;
 let log = Boolean;
 let headless = Boolean;
 let sitemapFile = String;
 let array = Array;
+let start = Date;
 
 async function crawler() {
    
@@ -198,13 +222,17 @@ async function crawler() {
    .version('0.1.0')
    .option('-u --url [url]', 'Add the site URL you want to generate a sitemap for. e.g. -u https://www.seleniumconf.com')
    .option('-s --sitemap [sitemap]', 'Use an already existing sitemap file. e.g. -s "/path/to/sitemap.xml" Note: This overrides the -u arg')
-   .option('-b, --browsers [browsers]', 'Add the MAX number of browsers to run concurrently. e.g. -b 10. Note: Be careful with this!', parseInt)
    .option('-m, --sitemapUrl [sitemapUrl', 'Specify a sitemap URL. e.g. -m https://www.example.com/sitemap.xml')
-   .option('--no-grid', 'Disable the Visual Grid and run locally only (Default: true). e.g. --no-grid')
+   .option('-b, --browsers [browsers]', 'Add the MAX number of browsers to run concurrently. e.g. -b 10. Note: Be careful with this!', parseInt)
+   .option('-k --key [key]', 'Set your Applitools API Key. e.g. -k yourLongAPIKeyyyyy')
+   .option('-v --serverUrl [serverUrl]', 'Set your Applitools on-prem or private cloud server URL. (Default: https://eyes.applitools.com). e.g. -v https://youreyes.applitools.com')
+   .option('--no-grid', 'Disable the Visual Grid and run locally only (Default: false). e.g. --no-grid')
    .option('--log', 'Enable Applitools Debug Logs (Default: false). e.g. --log')
    .option('--headless', 'Run Chrome headless (Default: false). e.g. --headless')
    .parse(process.argv);
    
+   apiKey = program.key
+   serverUrl = program.serverUrl
    enableVisualGrid = program.grid;
    log = program.log;
    headless = program.headless;
@@ -230,6 +258,9 @@ async function crawler() {
       
       array = await sitemapArray(sitemapFile);
    }
+   
+   var start = new Date();
+   console.log("\nStart Time: " + start);
    
    const PromisePool = require('es6-promise-pool');
    const pool = new PromisePool(promiseProducer, program.browsers);
